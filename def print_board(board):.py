95e9478d0c8e538b031c137b7109f91c03a21bbe
EliB5321 <113954789+EliB5321@users.noreply.github.com>
2023-06-05 01:51:19 -0700

    Add files via upload
    A Python script that runs tic-tac-toe

---
+++ def print_board(board):.py
@@ -0,0 +1,55 @@
+def print_board(board):
+    for row in board:
+        print("|".join(row))
+        print("-" * 5)
+
+def check_winner(board):
+    # Check rows
+    for row in board:
+        if row.count(row[0]) == len(row) and row[0] != " ":
+            return row[0]
+
+    # Check columns
+    for col in range(len(board[0])):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != " ":
+            return board[0][col]
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != " ":
+        return board[0][2]
+
+    return None
+
+def play_game():
+    board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+    current_player = "X"
+
+    while True:
+        print_board(board)
+        print(f"Player {current_player}'s turn.")
+
+        row = int(input("Enter the row (0-2): "))
+        col = int(input("Enter the column (0-2): "))
+
+        if board[row][col] != " ":
+            print("Invalid move. Try again.")
+            continue
+
+        board[row][col] = current_player
+
+        winner = check_winner(board)
+        if winner:
+            print_board(board)
+            print(f"Player {winner} wins!")
+            break
+
+        if all(all(cell != " " for cell in row) for row in board):
+            print_board(board)
+            print("It's a tie!")
+            break
+
+        current_player = "O" if current_player == "X" else "X"
+
+play_game()
